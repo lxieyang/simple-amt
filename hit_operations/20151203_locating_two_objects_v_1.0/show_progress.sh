2d11803ad75623aeb2f202802e6de50dc48376aa
Xieyang Liu <lxieyang@umich.edu>
2015-12-03 15:31:11 -0500

    show_progress.sh in the 20151203 template file is updated to refelct the changes.

--- hit_operations/20151203_locating_two_objects_v_1.0/show_progress.sh
+++ hit_operations/20151203_locating_two_objects_v_1.0/show_progress.sh
@@ -1,1 +1,1 @@
-python show_hit_progress.py --hit_ids_file=examples/locating_two_objects/hit_ids.txt
+python show_hit_progress.py --hit_ids_file=hit_operations/20151203_locating_two_objects_v_1.0/hit_ids.txt
