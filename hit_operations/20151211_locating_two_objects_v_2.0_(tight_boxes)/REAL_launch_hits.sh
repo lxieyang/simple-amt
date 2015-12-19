9107ae6871a05f43371462c97b03d474595cbc9b
Xieyang Liu <lxieyang@umich.edu>
2015-12-19 00:26:46 -0500

    preliminary setup of two rect version

--- hit_operations/20151211_locating_two_objects_v_2.0_(tight_boxes)/REAL_launch_hits.sh
+++ hit_operations/20151211_locating_two_objects_v_2.0_(tight_boxes)/REAL_launch_hits.sh
@@ -1,5 +1,5 @@
 python launch_hits.py \
-  --html_template=20151211_locating_two_objects_v_2.0_(tight_boxes).html \
+  --html_template=20151211_locating_two_objects_v_2.0_tight_boxes.html \
   --hit_properties_file=hit_properties/20151211_REAL_locating_two_objects_v_2.0_(tight_boxes).json \
   --input_json_file=hit_operations/20151211_locating_two_objects_v_2.0_(tight_boxes)/example_input.txt \
   --hit_ids_file=hit_operations/20151211_locating_two_objects_v_2.0_(tight_boxes)/REAL_hit_ids.txt --prod
--- hit_operations/20151211_locating_two_objects_v_2.0_(tight_boxes)/render_template.sh
+++ hit_operations/20151211_locating_two_objects_v_2.0_(tight_boxes)/render_template.sh
@@ -1,1 +1,1 @@
-python render_template.py --html_template=20151211_locating_two_objects_v_2.0_(tight_boxes).html
+python render_template.py --html_template=20151211_locating_two_objects_v_2.0_tight_boxes.html
